2a185063ba9d82107aeabcfa0592cd2e7d6c484c
derfreaker <116462998+derfreaker@users.noreply.github.com>
2024-10-08 14:09:18 +0200

    stars

--- index.js
+++ index.js
@@ -29,3 +29,23 @@ languageIcons.forEach(icon =>{
         tooltip.style.top = `${event.pageY + 10}px`;
     })
 })
+
+function stars() {
+    let e = document.createElement("div");
+    e.setAttribute("class", "star");
+    document.body.appendChild(e);
+    e.style.left = Math.random() * +innerWidth + "px";
+
+    let size = Math.random() * 4;
+    let duration = Math.random() * 2;
+
+    e.style.fontSize = size + "px";
+    e.style.animationDuration = 2 + duration + "s";
+    setTimeout(function () {
+        document.body.removeChild(e);
+    }, 5000);
+}
+
+setInterval(function () {
+    stars();
+}, 30);
